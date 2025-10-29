3334e1254d264821d958ff209aed4479ba94c010
kadir <kadir19kadir80@gmail.com>
2025-10-29 11:08:39 +0100

    flask 01-02 project added

---
+++ Flask-01-02-Hello-JinjaTemplate/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,24 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+     return 'Hello world Flask'
+
+@app.route('/second')
+def second():
+     return 'This is the second page'
+
+@app.route('/third')
+def third():
+     return 'This is the third page'
+
+@app.route('/fourth/<string:id>') #http://127.0.0.1:5000/fourth/kadirstringiniburaya%20yazdim
+def forth(id):
+     return f'Id of this page is {id}'
+
+
+if __name__ == '__main__':
+     # app.run(host= '0.0.0.0', port=80) # for ec2 instance
+     app.run(debug=True) # for localhost
+     
---
+++ Flask-01-02-Hello-JinjaTemplate/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1= 5, number2= 11)
+
+@app.route('/mult')
+def number():
+    x=5
+    y=11
+    return render_template('body.html', value1=x, value2=y, mult=x*y)
+
+
+
+if __name__== "__main__":
+     # app.run(host= '0.0.0.0', port=80)  # for ec2 instances
+     app.run(debug=True, port=3000) # for localhost
